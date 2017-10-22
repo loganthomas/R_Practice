811785bf0f002a497bedec7d47cfd73625fa4b30
Logan Thomas <logan.thomas005@gmail.com>
2017-10-22 14:48:44 -0500

    Modified: updated docstring and included return

--- add2.r
+++ add2.r
@@ -1,3 +1,9 @@
-add2<-function(x,y){
-  x+y
+add2 <- function(x, y){
+    #' Computes the sum between two values
+    #'
+    #' @param x First of two values whose sum is to be calculated.
+    #' @param y Second of two values whose sum is to be calculated.
+    #' 
+    #' @return The sum of x and y.
+  return(x + y)
 }
