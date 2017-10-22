9572837d9807eb18b46262d986e4490115de5bd2
Logan Thomas <logan.thomas005@gmail.com>
2017-10-22 16:00:43 -0500

    Modified: updated func to take n and default to 10

--- above10.r
+++ above10.r
@@ -1,9 +1,11 @@
-above10 <- function(x){
-    #' Returns all values in a sequence or vector that are greater than 10.
+above_n <- function(x, n=10){
+    #' Returns all values in a sequence or vector that are greater than n.
+    #' If not specified, n defauls to 10.
     #'
-    #' @param x Sequence or vector of numbers to determine which values exceed 10.
+    #' @param x Sequenceh or vector of numbers to determine which values exceed n.
+    #' @param n Limit to apply to x. If not specified, default is 10.
     #' 
-    #' @return All values in x that exceed 10.
-    vals = x > 10
+    #' @return All values in x that exceed n.
+    vals = x > n
     return(x[vals])
 }
